83e6f91b2890acef26e2b1422750bb2a5e6f0fc4
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-08 18:28:35 +0400

    validate forms

--- app.js
+++ app.js
@@ -195,3 +195,23 @@ function reactTo(event, info) {
 function defaultMessage(info) {
   info.innerHTML = "Please enter your favorite coding language";
 }
+
+(function () {
+  const form = document.getElementById("contact");
+  form.addEventListener("submit", function (event) {
+    validate(form, event);
+  });
+})();
+function validate(form, event) {
+  let value = form.elements["Name"].value;
+  if (value === "") {
+    alert("Please Enter Your Name");
+    event.preventDefault();
+    return;
+  }
+  value = form.elements["Email"].value;
+  if (value === "" || value.indexOf("@") === -1 || value.indexOf(".") === -1) {
+    alert("Please Enter A Valid Email Address");
+    event.preventDefault();
+  }
+}
