5a6c768c37d257eaee66f8685d84f6a58cb85f6c
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-08 16:24:15 +0400

    address elements

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ list.innerHTML += "<li>No. Scripts: " + document.scripts.length;
 list.innerHTML += "<li>First Image URL:" + document.images[0].src;
 list.innerHTML +=
   "<li>First Form Element Value: " + document.forms[0].elements[0].value;
+
+const info = document.getElementById("info");
+const item = document.getElementById("country");
+const lists = document.getElementsByTagName("ol");
+const fruits = document.getElementsByClassName("fruit");
+let i = 0;
+info.innerHTML = item + " Id: " + item.innerText + "<br>";
+info.innerHTML += "<br>" + lists + " Tags:<br>";
+for (i = 0; i < lists.length; i++) {
+  info.innerHTML += i + 1 + " of " + lists.length;
+  info.innerHTML += " : " + lists[i].innerText + "<br>";
+}
+info.innerHTML += "<br>" + fruits + " Class:<br>";
+for (i = 0; i < fruits.length; i++) {
+  info.innerHTML += i + 1 + " of " + fruits.length;
+  info.innerHTML += " : " + fruits[i].innerText + "<br>";
+}
