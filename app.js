02c79c82b5c105a5c70a50e9070adfbd97cec6b5
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-08 17:18:16 +0400

    mouse events

--- app.js
+++ app.js
@@ -82,3 +82,27 @@ function welcome() {
     info.innerHTML = "Welcome, " + name;
   }
 }
+
+(function () {
+  const box = document.getElementById("box");
+  const btn = document.getElementById("btn");
+  box.addEventListener("mouseover", function (event) {
+    reactTo(event, "Red");
+  });
+  box.addEventListener("mouseout", function (event) {
+    reactTo(event, "Purple");
+  });
+  box.addEventListener("mousedown", function (event) {
+    reactTo(event, "Green");
+  });
+  box.addEventListener("mouseup", function (event) {
+    reactTo(event, "Blue");
+  });
+  btn.addEventListener("click", function (event) {
+    reactTo(event, "Orange");
+  });
+})();
+function reactTo(event, color) {
+  document.getElementById("box").style.background = color;
+  document.getElementById("infoa").innerText = event.type;
+}
