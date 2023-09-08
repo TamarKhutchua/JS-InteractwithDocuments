ed32d3e66f10c45b85187d7d6fc8e96b6cf73a21
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-08 18:02:33 +0400

    check boxes

--- app.js
+++ app.js
@@ -132,3 +132,25 @@ function reactTo(event) {
       "<br>" + event.type + ": " + String.fromCharCode(event.keyCode) + "<hr>";
   }
 }
+
+(function () {
+  const form = document.getElementById("pizza");
+  form.addEventListener("submit", function (event) {
+    reactTo(form, event);
+  });
+  form.Top[0].checked = true;
+})();
+function reactTo(form, event) {
+  let i,
+    ok,
+    summary = "";
+  for (i = 0; i < form.Top.length; i++) {
+    if (form.Top[i].checked) {
+      summary += form.Top[i].value + "";
+    }
+  }
+  ok = confirm("Submit These Choices?\n" + summary);
+  if (!ok) {
+    event.preventDefault();
+  }
+}
