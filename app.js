20695803e75107d1e9d81cc04b870dc7211621f4
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-08 17:00:04 +0400

    manage cookies

--- app.js
+++ app.js
@@ -5,39 +5,69 @@
 // list.innerHTML += "<li>Domain: " + document.domain;
 // list.innerHTML += "<li>Last Modified: " + document.lastModified;
 
-const list = document.getElementById("list");
-list.innerHTML = "<li>No. Forms: " + document.forms.length;
-list.innerHTML += "<li>No. Links: " + document.links.length;
-list.innerHTML += "<li>No. Images: " + document.images.length;
-list.innerHTML += "<li>No. Style Sheets: " + document.styleSheets.length;
-list.innerHTML += "<li>No. Scripts: " + document.scripts.length;
-list.innerHTML += "<li>First Image URL:" + document.images[0].src;
-list.innerHTML +=
-  "<li>First Form Element Value: " + document.forms[0].elements[0].value;
+// const list = document.getElementById("list");
+// list.innerHTML = "<li>No. Forms: " + document.forms.length;
+// list.innerHTML += "<li>No. Links: " + document.links.length;
+// list.innerHTML += "<li>No. Images: " + document.images.length;
+// list.innerHTML += "<li>No. Style Sheets: " + document.styleSheets.length;
+// list.innerHTML += "<li>No. Scripts: " + document.scripts.length;
+// list.innerHTML += "<li>First Image URL:" + document.images[0].src;
+// list.innerHTML +=
+//   "<li>First Form Element Value: " + document.forms[0].elements[0].value;
 
-const info = document.getElementById("info");
-const item = document.getElementById("country");
-const lists = document.getElementsByTagName("ol");
-const fruits = document.getElementsByClassName("fruit");
-let i = 0;
-info.innerHTML = item + " Id: " + item.innerText + "<br>";
-info.innerHTML += "<br>" + lists + " Tags:<br>";
-for (i = 0; i < lists.length; i++) {
-  info.innerHTML += i + 1 + " of " + lists.length;
-  info.innerHTML += " : " + lists[i].innerText + "<br>";
-}
-info.innerHTML += "<br>" + fruits + " Class:<br>";
-for (i = 0; i < fruits.length; i++) {
-  info.innerHTML += i + 1 + " of " + fruits.length;
-  info.innerHTML += " : " + fruits[i].innerText + "<br>";
-}
+// const info = document.getElementById("info");
+// const item = document.getElementById("country");
+// const lists = document.getElementsByTagName("ol");
+// const fruits = document.getElementsByClassName("fruit");
+// let i = 0;
+// info.innerHTML = item + " Id: " + item.innerText + "<br>";
+// info.innerHTML += "<br>" + lists + " Tags:<br>";
+// for (i = 0; i < lists.length; i++) {
+//   info.innerHTML += i + 1 + " of " + lists.length;
+//   info.innerHTML += " : " + lists[i].innerText + "<br>";
+// }
+// info.innerHTML += "<br>" + fruits + " Class:<br>";
+// for (i = 0; i < fruits.length; i++) {
+//   info.innerHTML += i + 1 + " of " + fruits.length;
+//   info.innerHTML += " : " + fruits[i].innerText + "<br>";
+// }
 
-const itemFour = document.createElement("li");
-const itemFive = document.createElement("li");
-const heading = document.getElementById("heading");
-itemFour.innerText = "London, England";
-itemFive.innerText = "Cape Town, South Africa";
-document.getElementById("listi").appendChild(itemFour);
-document.getElementById("listi").appendChild(itemFive);
-heading.setAttribute("style", "color:Red");
-heading.innerText = "Best Five Cities";
+// const itemFour = document.createElement("li");
+// const itemFive = document.createElement("li");
+// const heading = document.getElementById("heading");
+// itemFour.innerText = "London, England";
+// itemFive.innerText = "Cape Town, South Africa";
+// document.getElementById("listi").appendChild(itemFour);
+// document.getElementById("listi").appendChild(itemFive);
+// heading.setAttribute("style", "color:Red");
+// heading.innerText = "Best Five Cities";
+
+function setCookie(key, value, days) {
+  const d = new Date();
+  d.setTime(d.getTime() + days * 86400000);
+  document.cookie =
+    key + "=" + encodeURI(value) + ";expires=" + d.toUTCString() + ";";
+}
+function getCookie(key) {
+  if (document.cookie) {
+    const pairs = decodeURI(document.cookie).split(";");
+    let i, name, value;
+    for (i = 0; i < pairs.length; i++) {
+      name = pairs[i].split("=")[0].trim();
+      if (name === key) {
+        value = pairs[i].split("=")[1];
+      }
+    }
+    return value;
+  }
+}
+setCookie("User", "Mike McGrath,12345", 7);
+const list = document.getElementById("lista");
+let i,
+  value = getCookie("User");
+if (value.indexOf(",")) {
+  value = value.split(",");
+}
+for (i = 0; i < value.length; i++) {
+  list.innerHTML += "<li>" + value[i];
+}
